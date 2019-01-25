52df2403623dd2fac1727d6c3b50872689f56e66
Robin Hillyard <scalaprof@gmail.com>
2019-01-25 09:51:21 -0500

    Updated Benchmark class
    Added fPre and fPost, etc. to Benchmark glass

--- src/main/java/edu/neu/coe/info6205/BinarySearch.java
+++ src/main/java/edu/neu/coe/info6205/BinarySearch.java
@@ -1,10 +1,14 @@
 package edu.neu.coe.info6205;
 
+import java.util.Arrays;
+
 public class BinarySearch {
     public static void main (String args[]){
         int[] ar = {1,2,3,4,5,6,7,8,9};
-        int res = binarySearch(ar,0,ar.length-1,3);
-        System.out.println(res);
+        int res1 = binarySearch(ar,0,ar.length-1,3);
+        System.out.println(res1);
+        int res2 = Arrays.binarySearch(ar, 0, ar.length, 3);
+        System.out.println(res2);
     }
 
 
